47fd5b42a7817d42b7b5d041325dfccb2f6441ca
tb2706 <bierbaum.tim@outlook.de>
2019-09-23 19:06:00 +0200

    add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,33 @@
-import 'package:flutter/material.dart';
-import 'package:superbingo/blocs/game_bloc.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+
+import 'package:superbingo/blocs/game_bloc.dart';
 import 'package:superbingo/blocs/open_games_bloc.dart';
 import 'package:superbingo/superbingo.dart';
 
 import 'package:provider/provider.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async {
-  runApp(
-    MultiProvider(
-      child: SuperBingo(),
-      providers: [
-        Provider<PublicGamesBloc>(
-          builder: (_) => PublicGamesBloc(),
-          dispose: (_, bloc) => bloc.dispose(),
-        ),
-        Provider<GameBloc>(
-          builder: (_) => GameBloc(),
-          dispose: (_, bloc) => bloc.dispose(),
-        ),
-      ],
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runZoned(
+    () => runApp(
+      MultiProvider(
+        child: SuperBingo(),
+        providers: [
+          Provider<PublicGamesBloc>(
+            builder: (_) => PublicGamesBloc(),
+            dispose: (_, bloc) => bloc.dispose(),
+          ),
+          Provider<GameBloc>(
+            builder: (_) => GameBloc(),
+            dispose: (_, bloc) => bloc.dispose(),
+          ),
+        ],
+      ),
     ),
+    onError: Crashlytics.instance.recordError,
   );
 }
