aef7ebb06ae07316603f53dd6f1cc2182b5982c0
tb2761 <tb2706@outlook.de>
2019-12-27 21:25:20 +0100

    move visualEffects building to initState and didChangeDependencies,

--- lib/widgets/card_stack.dart
+++ lib/widgets/card_stack.dart
@@ -1,21 +1,35 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:superbingo/bloc/blocs/current_game_bloc.dart';
 import 'package:superbingo/bloc/events/current_game_events.dart' as events;
-
 import 'package:superbingo/models/app_models/card.dart';
 import 'package:superbingo/widgets/play_card.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vector_math/vector_math.dart' show radians;
 
-enum CardStackType { playedCards, unplayedCards }
+enum CardStackType {
+  /// Stapel der gespielten Karten
+  playedCards,
 
-class CardStack extends StatelessWidget {
+  /// Nachziehstapel
+  unplayedCards
+}
+
+/// {@template cardstack}
+/// [CardStack] stellt einen Kartenstapel dar und fügt den Spielkarten, die nötigen visuellen
+/// Effekte hinzu, damit der Kartenstapel realistisch aussieht.
+/// {@endtemplate}
+class CardStack extends StatefulWidget {
+  /// Konfiguration, ob es sich um den Nachziehstapel oder um den Stapel der gelegten Karten handelt
   final CardStackType type;
+
+  /// Spielkarten, die auf diesem Kartenstapel liegen
   final List<GameCard> cards;
 
+  /// {@macro cardstack}
   const CardStack({
     Key key,
     @required this.type,
@@ -25,61 +39,103 @@ class CardStack extends StatelessWidget {
         super(key: key);
 
   @override
+  _CardStackState createState() => _CardStackState();
+}
+
+class _CardStackState extends State<CardStack> {
+  List<Map<String, dynamic>> cards;
+
+  @override
+  void initState() {
+    super.initState();
+    buildVisualEffects();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    buildVisualEffects();
+  }
+
+  @override
+  void didUpdateWidget(CardStack oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.cards.length != widget.cards.length) {
+      buildVisualEffects();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final currentGameBloc = BlocProvider.of<CurrentGameBloc>(context);
 
     return Container(
       child: Stack(
         fit: StackFit.loose,
-        children: cards.map<Widget>((c) {
-          final rn = Random();
-          final index = cards.indexOf(c);
-          var angle = 1.0 + rn.nextInt(10);
-          double translationY, translationX;
-          var elevation = 0.0;
-          if (c == cards.last) {
-            angle = radians(0);
-          } else {
-            angle = radians(angle);
-            angle = index & 1 == 0 ? angle : -angle;
-          }
-          translationY = 1.0 + rn.nextInt(5);
-          translationX = 1.0 + rn.nextInt(5);
-          if (rn.nextDouble() <= 0.5) {
-            translationX = -translationX;
-          }
-          if (rn.nextDouble() <= 0.5) {
-            translationY = -translationY;
-          }
-
-          if (index > cards.length - 10) {
-            elevation = index - (cards.length - 10.0);
-          }
-
-          return Transform(
-            child: Transform.rotate(
-              child: PlayCard(
-                card: c,
-                index: index,
-                elevation: elevation,
-                angle: angle,
-                isFlipped: type == CardStackType.unplayedCards,
-                onCardTap: (card) {
-                  if (type == CardStackType.unplayedCards) {
-                    currentGameBloc.add(events.PullCard(card));
-                  }
-                },
+        children: cards
+            .map<Widget>(
+              (c) => Transform(
+                child: Transform.rotate(
+                  child: PlayCard(
+                    card: c['card'],
+                    index: c['index'],
+                    elevation: c['elevation'],
+                    isFlipped: widget.type == CardStackType.unplayedCards,
+                    onCardTap: (card) {
+                      if (widget.type == CardStackType.unplayedCards) {
+                        currentGameBloc.add(events.PullCard(card));
+                      }
+                    },
+                  ),
+                  angle: c['angle'],
+                ),
+                transform: Matrix4.identity()
+                  ..translate(
+                    c['translationX'],
+                    c['translationY'],
+                  ),
               ),
-              angle: angle,
-            ),
-            transform: Matrix4.identity()
-              ..translate(
-                translationX,
-                translationY,
-              ),
-          );
-        }).toList(),
+            )
+            .toList(),
       ),
     );
+  }
+
+  void buildVisualEffects() {
+    final rn = Random();
+    cards = widget.cards.map<Map<String, dynamic>>(
+      (c) {
+        final index = widget.cards.indexOf(c);
+        double translationY, translationX, elevation = 0.0, angle = 0.0;
+        if (c == widget.cards.last) {
+          angle = radians(0);
+        } else {
+          angle = 1.0 + rn.nextInt(10);
+          angle = radians(angle);
+          angle = index & 1 == 0 ? angle : -angle;
+        }
+        translationY = 1.0 + rn.nextInt(5);
+        translationX = 1.0 + rn.nextInt(5);
+        if (rn.nextDouble() < 0.5) {
+          translationX = -translationX;
+        }
+        if (rn.nextDouble() < 0.5) {
+          translationY = -translationY;
+        }
+
+        if (index > widget.cards.length - 10) {
+          elevation = index - (widget.cards.length - 10.0);
+        }
+
+        return <String, dynamic>{
+          'card': c,
+          'index': index,
+          'elevation': elevation,
+          'angle': angle,
+          'translationX': translationX,
+          'translationY': translationY,
+        };
+      },
+    ).toList();
   }
 }
