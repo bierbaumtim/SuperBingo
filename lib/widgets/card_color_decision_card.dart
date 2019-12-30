38082e841a0a250084acd4bfa6199cf5da6af939
tb2761 <tb2706@outlook.de>
2019-12-30 20:19:47 +0100

    added overlay for cardColor selection when playing joker or jack cards

---
+++ lib/widgets/card_color_decision_card.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import 'package:superbingo/constants/enums.dart';
+
+import 'package:community_material_icon/community_material_icon.dart';
+
+class CardColorDecisionCard extends StatefulWidget {
+  const CardColorDecisionCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _CardColorDecisionCardState createState() => _CardColorDecisionCardState();
+}
+
+class _CardColorDecisionCardState extends State<CardColorDecisionCard> {
+  CardColor color;
+
+  @override
+  void initState() {
+    super.initState();
+    color = CardColor.clover;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedColor = Colors.deepOrangeAccent;
+
+    return Center(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: Text('Welche Farbe wünschst du dir ?'),
+              ),
+              RadioListTile(
+                controlAffinity: ListTileControlAffinity.trailing,
+                value: CardColor.clover,
+                groupValue: color,
+                title: Text('Kreuz'),
+                secondary: Icon(CommunityMaterialIcons.cards_club),
+                onChanged: (value) => setState(() => color = value),
+                activeColor: selectedColor,
+              ),
+              RadioListTile(
+                controlAffinity: ListTileControlAffinity.trailing,
+                value: CardColor.spade,
+                groupValue: color,
+                title: Text('Pik'),
+                secondary: Icon(CommunityMaterialIcons.cards_spade),
+                onChanged: (value) => setState(() => color = value),
+                activeColor: selectedColor,
+              ),
+              RadioListTile(
+                controlAffinity: ListTileControlAffinity.trailing,
+                value: CardColor.heart,
+                groupValue: color,
+                title: Text('Herz'),
+                secondary: Icon(CommunityMaterialIcons.cards_heart),
+                onChanged: (value) => setState(() => color = value),
+                activeColor: selectedColor,
+              ),
+              RadioListTile(
+                controlAffinity: ListTileControlAffinity.trailing,
+                value: CardColor.diamond,
+                groupValue: color,
+                title: Text('Karo'),
+                secondary: Icon(CommunityMaterialIcons.cards_diamond),
+                onChanged: (value) => setState(() => color = value),
+                activeColor: selectedColor,
+              ),
+              SizedBox(height: 24),
+              RaisedButton(
+                child: Text('Wünschen'),
+                onPressed: () => Navigator.of(context).pop(color),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/horizontal_card_listview.dart
+++ lib/widgets/horizontal_card_listview.dart
@@ -1,8 +1,12 @@
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 
 import 'package:superbingo/bloc/blocs/current_game_bloc.dart';
 import 'package:superbingo/bloc/events/current_game_events.dart';
+import 'package:superbingo/constants/enums.dart';
 import 'package:superbingo/models/app_models/card.dart';
+import 'package:superbingo/routes/blur_overlay_route.dart';
+import 'package:superbingo/widgets/card_color_decision_card.dart';
 import 'package:superbingo/widgets/small_play_card.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,9 +34,19 @@ class HorizontalCardList extends StatelessWidget {
           height: 175,
           child: ListView.builder(
             itemBuilder: (context, index) => SmallPlayCard(
-              card: cards.elementAt(index),
-              onCardTap: (card) => currentGameBloc.add(PlayCard(card)),
-            ),
+                card: cards.elementAt(index),
+                onCardTap: (card) async {
+                  CardColor allowedCardColor;
+                  if (card.rule == SpecialRule.joker) {
+                    final route = BlurOverlayRoute<CardColor>(
+                      builder: (context) => CardColorDecisionCard(),
+                    );
+                    allowedCardColor =
+                        await Navigator.of(context).push<CardColor>(route);
+                    if (allowedCardColor == null) return;
+                  }
+                  currentGameBloc.add(PlayCard(card, allowedCardColor));
+                }),
             itemCount: cards.length,
             scrollDirection: Axis.horizontal,
             physics: ClampingScrollPhysics(),
