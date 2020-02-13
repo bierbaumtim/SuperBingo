328e8705f24006499bccd3409cfa8b0ee2db9084
tb2761 <tb2706@outlook.de>
2020-02-13 19:36:41 +0100

    added loading widget

---
+++ lib/widgets/loading_widget.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  final String content;
+  final EdgeInsets padding;
+
+  const Loading({
+    Key key,
+    this.content = 'Laden',
+    this.padding = const EdgeInsets.only(top: 8),
+  })  : assert(content != null),
+        assert(padding != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CupertinoActivityIndicator(),
+            const SizedBox(width: 8),
+            Text(content),
+          ],
+        ),
+      );
+    }
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const CircularProgressIndicator(),
+          Padding(
+            padding: padding,
+            child: Material(
+              color: Colors.transparent,
+              elevation: 0,
+              child: Text(
+                content,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
