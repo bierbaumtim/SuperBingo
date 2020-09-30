cff5a85314cec533f0aeaa39fb3c4b48953e9228
bierbaumtim <tb2706@outlook.de>
2020-09-30 16:10:35 +0200

    update to Android Embedded V2

--- android/app/src/main/kotlin/bierbaum/com/superbingo/MainActivity.kt
+++ android/app/src/main/kotlin/bierbaum/com/superbingo/MainActivity.kt
@@ -1,36 +1,29 @@
 package bierbaum.com.superbingo
 
-import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity;
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+class MainActivity : FlutterActivity() {
+  // override fun onCreate(savedInstanceState: Bundle?) {
+  //   super.onCreate(savedInstanceState)
+  //   val flutter_native_splash = true
+  //   var originalStatusBarColor = 0
+  //   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+  //       originalStatusBarColor = window.statusBarColor
+  //       window.statusBarColor = 0xfff06135.toInt()
+  //   }
+  //   val originalStatusBarColorFinal = originalStatusBarColor
 
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
+  //   GeneratedPluginRegistrant.registerWith(this)
+  //   val vto = flutterView.viewTreeObserver
+  //   vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+  //     override fun onGlobalLayout() {
+  //       flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+  //       window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+  //       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+  //         window.statusBarColor = originalStatusBarColorFinal
+  //       }
+  //     }
+  //   })
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    val flutter_native_splash = true
-    var originalStatusBarColor = 0
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        originalStatusBarColor = window.statusBarColor
-        window.statusBarColor = 0xfff06135.toInt()
-    }
-    val originalStatusBarColorFinal = originalStatusBarColor
-
-    GeneratedPluginRegistrant.registerWith(this)
-    val vto = flutterView.viewTreeObserver
-    vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-      override fun onGlobalLayout() {
-        flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
-        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-          window.statusBarColor = originalStatusBarColorFinal
-        }
-      }
-    })
-
-  }
+  // }
 }
