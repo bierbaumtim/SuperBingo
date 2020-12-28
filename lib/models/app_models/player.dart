99a507efc48207ef29a50e30b166f91bc2fc347a
bierbaumtim <tb2706@outlook.de>
2020-12-28 22:47:08 +0100

    some small code improvements

--- lib/models/app_models/player.dart
+++ lib/models/app_models/player.dart
@@ -106,7 +106,7 @@ class Player with EquatableMixin {
     assert(id != null);
     final activePlayer =
         player.filter((player) => player.finishPosition == 0).toList();
-    var index = activePlayer.indexWhere((p) => p.id == id) ?? -1;
+    var index = activePlayer?.indexWhere((p) => p.id == id) ?? -1;
     index = skipNextPlayer ? index + 2 : index + 1;
     if (index > activePlayer.length - 1) {
       index = 0;
