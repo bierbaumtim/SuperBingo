498547ed1e406b9ad1a0df5bfb05af10abcea74b
bierbaumtim <tb2706@outlook.de>
2020-12-28 15:44:39 +0100

    Tests: fix issues in Tests

--- test/utils/utils_test.dart
+++ test/utils/utils_test.dart
@@ -16,137 +16,137 @@ void main() {
       group('getPositionCoordinates tests ->', () {
         test('1 player', () {
           final coordinates = getPositionCoordinates(0, 1, 350);
-          expect(coordinates['top'], 0);
-          expect(coordinates['left'], 0);
-          expect(coordinates['right'], null);
-          expect(coordinates['bottom'], null);
+          expect(coordinates.top, 0);
+          expect(coordinates.left, 0);
+          expect(coordinates.right, null);
+          expect(coordinates.bottom, null);
         });
 
         test('2 player', () {
           final coordinates1 = getPositionCoordinates(0, 2, 350);
-          expect(coordinates1['top'], null);
-          expect(coordinates1['left'], 0);
-          expect(coordinates1['right'], null);
-          expect(coordinates1['bottom'], null);
+          expect(coordinates1.top, null);
+          expect(coordinates1.left, 0);
+          expect(coordinates1.right, null);
+          expect(coordinates1.bottom, null);
 
           final coordinates2 = getPositionCoordinates(1, 2, 350);
-          expect(coordinates2['top'], null);
-          expect(coordinates2['left'], null);
-          expect(coordinates2['right'], 0);
-          expect(coordinates2['bottom'], null);
+          expect(coordinates2.top, null);
+          expect(coordinates2.left, null);
+          expect(coordinates2.right, 0);
+          expect(coordinates2.bottom, null);
         });
 
         test('3 player', () {
           final coordinates1 = getPositionCoordinates(0, 3, 350);
-          expect(coordinates1['top'], null);
-          expect(coordinates1['left'], 0);
-          expect(coordinates1['right'], null);
-          expect(coordinates1['bottom'], 0);
+          expect(coordinates1.top, null);
+          expect(coordinates1.left, 0);
+          expect(coordinates1.right, null);
+          expect(coordinates1.bottom, 0);
 
           final coordinates2 = getPositionCoordinates(1, 3, 350);
-          expect(coordinates2['top'], null);
-          expect(coordinates2['left'], 0);
-          expect(coordinates2['right'], null);
-          expect(coordinates2['bottom'], null);
+          expect(coordinates2.top, null);
+          expect(coordinates2.left, 0);
+          expect(coordinates2.right, null);
+          expect(coordinates2.bottom, null);
 
           final coordinates3 = getPositionCoordinates(2, 3, 350);
-          expect(coordinates3['top'], null);
-          expect(coordinates3['left'], null);
-          expect(coordinates3['right'], 0);
-          expect(coordinates3['bottom'], null);
+          expect(coordinates3.top, null);
+          expect(coordinates3.left, null);
+          expect(coordinates3.right, 0);
+          expect(coordinates3.bottom, null);
         });
         test('4 player', () {
           final coordinates1 = getPositionCoordinates(0, 4, 350);
-          expect(coordinates1['top'], null);
-          expect(coordinates1['left'], 0);
-          expect(coordinates1['right'], null);
-          expect(coordinates1['bottom'], 0);
+          expect(coordinates1.top, null);
+          expect(coordinates1.left, 0);
+          expect(coordinates1.right, null);
+          expect(coordinates1.bottom, 0);
 
           final coordinates2 = getPositionCoordinates(1, 4, 350);
-          expect(coordinates2['top'], null);
-          expect(coordinates2['left'], 0);
-          expect(coordinates2['right'], null);
-          expect(coordinates2['bottom'], null);
+          expect(coordinates2.top, null);
+          expect(coordinates2.left, 0);
+          expect(coordinates2.right, null);
+          expect(coordinates2.bottom, null);
 
           final coordinates3 = getPositionCoordinates(2, 4, 350);
-          expect(coordinates3['top'], null);
-          expect(coordinates3['left'], null);
-          expect(coordinates3['right'], 0);
-          expect(coordinates3['bottom'], null);
+          expect(coordinates3.top, null);
+          expect(coordinates3.left, null);
+          expect(coordinates3.right, 0);
+          expect(coordinates3.bottom, null);
 
           final coordinates4 = getPositionCoordinates(3, 4, 350);
-          expect(coordinates4['top'], null);
-          expect(coordinates4['left'], null);
-          expect(coordinates4['right'], 0);
-          expect(coordinates4['bottom'], 0);
+          expect(coordinates4.top, null);
+          expect(coordinates4.left, null);
+          expect(coordinates4.right, 0);
+          expect(coordinates4.bottom, 0);
         });
         test('5 player', () {
           final coordinates1 = getPositionCoordinates(0, 5, 350);
-          expect(coordinates1['top'], null);
-          expect(coordinates1['left'], 0);
-          expect(coordinates1['right'], null);
-          expect(coordinates1['bottom'], 0);
+          expect(coordinates1.top, null);
+          expect(coordinates1.left, 0);
+          expect(coordinates1.right, null);
+          expect(coordinates1.bottom, 0);
 
           final coordinates2 = getPositionCoordinates(1, 5, 350);
-          expect(coordinates2['top'], 143);
-          expect(coordinates2['left'], 0);
-          expect(coordinates2['right'], null);
-          expect(coordinates2['bottom'], null);
+          expect(coordinates2.top, 143);
+          expect(coordinates2.left, 0);
+          expect(coordinates2.right, null);
+          expect(coordinates2.bottom, null);
 
           final coordinates3 = getPositionCoordinates(2, 5, 350);
-          expect(coordinates3['top'], null);
-          expect(coordinates3['left'], 0);
-          expect(coordinates3['right'], null);
-          expect(coordinates3['bottom'], null);
+          expect(coordinates3.top, null);
+          expect(coordinates3.left, 0);
+          expect(coordinates3.right, null);
+          expect(coordinates3.bottom, null);
 
           final coordinates4 = getPositionCoordinates(3, 5, 350);
-          expect(coordinates4['top'], null);
-          expect(coordinates4['left'], null);
-          expect(coordinates4['right'], 0);
-          expect(coordinates4['bottom'], null);
+          expect(coordinates4.top, null);
+          expect(coordinates4.left, null);
+          expect(coordinates4.right, 0);
+          expect(coordinates4.bottom, null);
 
           final coordinates5 = getPositionCoordinates(4, 5, 350);
-          expect(coordinates5['top'], 143);
-          expect(coordinates5['left'], null);
-          expect(coordinates5['right'], 0);
-          expect(coordinates5['bottom'], null);
+          expect(coordinates5.top, 143);
+          expect(coordinates5.left, null);
+          expect(coordinates5.right, 0);
+          expect(coordinates5.bottom, null);
         });
         test('6 player', () {
           final coordinates1 = getPositionCoordinates(0, 6, 350);
-          expect(coordinates1['top'], null);
-          expect(coordinates1['left'], 0);
-          expect(coordinates1['right'], null);
-          expect(coordinates1['bottom'], 0);
+          expect(coordinates1.top, null);
+          expect(coordinates1.left, 0);
+          expect(coordinates1.right, null);
+          expect(coordinates1.bottom, 0);
 
           final coordinates2 = getPositionCoordinates(1, 6, 350);
-          expect(coordinates2['top'], 143);
-          expect(coordinates2['left'], 0);
-          expect(coordinates2['right'], null);
-          expect(coordinates2['bottom'], null);
+          expect(coordinates2.top, 143);
+          expect(coordinates2.left, 0);
+          expect(coordinates2.right, null);
+          expect(coordinates2.bottom, null);
 
           final coordinates3 = getPositionCoordinates(2, 6, 350);
-          expect(coordinates3['top'], null);
-          expect(coordinates3['left'], 0);
-          expect(coordinates3['right'], null);
-          expect(coordinates3['bottom'], null);
+          expect(coordinates3.top, null);
+          expect(coordinates3.left, 0);
+          expect(coordinates3.right, null);
+          expect(coordinates3.bottom, null);
 
           final coordinates4 = getPositionCoordinates(3, 6, 350);
-          expect(coordinates4['top'], null);
-          expect(coordinates4['left'], null);
-          expect(coordinates4['right'], 0);
-          expect(coordinates4['bottom'], null);
+          expect(coordinates4.top, null);
+          expect(coordinates4.left, null);
+          expect(coordinates4.right, 0);
+          expect(coordinates4.bottom, null);
 
           final coordinates5 = getPositionCoordinates(4, 6, 350);
-          expect(coordinates5['top'], 143);
-          expect(coordinates5['left'], null);
-          expect(coordinates5['right'], 0);
-          expect(coordinates5['bottom'], null);
+          expect(coordinates5.top, 143);
+          expect(coordinates5.left, null);
+          expect(coordinates5.right, 0);
+          expect(coordinates5.bottom, null);
 
           final coordinates6 = getPositionCoordinates(5, 6, 350);
-          expect(coordinates6['top'], null);
-          expect(coordinates6['left'], null);
-          expect(coordinates6['right'], 0);
-          expect(coordinates6['bottom'], 0);
+          expect(coordinates6.top, null);
+          expect(coordinates6.left, null);
+          expect(coordinates6.right, 0);
+          expect(coordinates6.bottom, 0);
         });
       });
     });
